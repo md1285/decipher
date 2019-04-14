d5025c34f27c8a6147f12b741eeb473c205795cf
Your Name <you@example.com>
2019-04-13 22:28:33 -0700

    refator inline styling into conditional class

--- src/components/Descrambler/Descrambler.jsx
+++ src/components/Descrambler/Descrambler.jsx
@@ -8,184 +8,104 @@ class Descrambler extends React.Component {
       <div className="dials-container">
         <div className="descrambler-container descrambler-left-container">
           <button
-            className="dsc-btn dsc-lft-0"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 0 && 'green',
-              color: this.props.descramblerSettingLeft === 0 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-0 ${this.props.descramblerSettingLeft === 0 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="0">0</button>
           <button
-            className="dsc-btn dsc-lft-1"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 1 && 'green',
-              color: this.props.descramblerSettingLeft === 1 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-1 ${this.props.descramblerSettingLeft === 1 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="1">1</button>
           <button
-            className="dsc-btn dsc-lft-2"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 2 && 'green',
-              color: this.props.descramblerSettingLeft === 2 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-2 ${this.props.descramblerSettingLeft === 2 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="2">2</button>
           <button
-            className="dsc-btn dsc-lft-3"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 3 && 'green',
-              color: this.props.descramblerSettingLeft === 3 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-3 ${this.props.descramblerSettingLeft === 3 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="3">3</button>
           <button
-            className="dsc-btn dsc-lft-4"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 4 && 'green',
-              color: this.props.descramblerSettingLeft === 4 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-4 ${this.props.descramblerSettingLeft === 4 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="4">4</button>
           <button
-            className="dsc-btn dsc-lft-5"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 5 && 'green',
-              color: this.props.descramblerSettingLeft === 5 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-5 ${this.props.descramblerSettingLeft === 5 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="5">5</button>
           <button
-            className="dsc-btn dsc-lft-6"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 6 && 'green',
-              color: this.props.descramblerSettingLeft === 6 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-6 ${this.props.descramblerSettingLeft === 6 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="6">6</button>
           <button
-            className="dsc-btn dsc-lft-7"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 7 && 'green',
-              color: this.props.descramblerSettingLeft === 7 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-7 ${this.props.descramblerSettingLeft === 7 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="7">7</button>
           <button
-            className="dsc-btn dsc-lft-8"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 8 && 'green',
-              color: this.props.descramblerSettingLeft === 8 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-8 ${this.props.descramblerSettingLeft === 8 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="8">8</button>
           <button
-            className="dsc-btn dsc-lft-9"
-            style={{
-              backgroundColor: this.props.descramblerSettingLeft === 9 && 'green',
-              color: this.props.descramblerSettingLeft === 9 && 'white',
-            }}
+            className={`dsc-btn dsc-lft-9 ${this.props.descramblerSettingLeft === 9 && 'dsc-btn-sel'}`}
             name="descramblerSettingLeft"
             onClick={this.props.handleChange}
             value="9">9</button>
         </div>
         <div className="descrambler-container descrambler-right-container">
           <button
-            className="dsc-btn dsc-right-0"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 0 && 'green',
-              color: this.props.descramblerSettingRight === 0 && 'white',
-            }}
+            className={`dsc-btn dsc-right-0 ${this.props.descramblerSettingRight === 0 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="0">0</button>
           <button
-            className="dsc-btn dsc-right-1"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 1 && 'green',
-              color: this.props.descramblerSettingRight === 1 && 'white',
-            }}
+            className={`dsc-btn dsc-right-1 ${this.props.descramblerSettingRight === 1 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="1">1</button>
           <button
-            className="dsc-btn dsc-right-2"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 2 && 'green',
-              color: this.props.descramblerSettingRight === 2 && 'white',
-            }}
+            className={`dsc-btn dsc-right-2 ${this.props.descramblerSettingRight === 2 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="2">2</button>
           <button
-            className="dsc-btn dsc-right-3"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 3 && 'green',
-              color: this.props.descramblerSettingRight === 3 && 'white',
-            }}
+            className={`dsc-btn dsc-right-3 ${this.props.descramblerSettingRight === 3 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="3">3</button>
           <button
-            className="dsc-btn dsc-right-4"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 4 && 'green',
-              color: this.props.descramblerSettingRight === 4 && 'white',
-            }}
+            className={`dsc-btn dsc-right-4 ${this.props.descramblerSettingRight === 4 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="4">4</button>
           <button
-            className="dsc-btn dsc-right-5"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 5 && 'green',
-              color: this.props.descramblerSettingRight === 5 && 'white',
-            }}
+            className={`dsc-btn dsc-right-5 ${this.props.descramblerSettingRight === 5 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="5">5</button>
           <button
-            className="dsc-btn dsc-right-6"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 6 && 'green',
-              color: this.props.descramblerSettingRight === 6 && 'white',
-            }}
+            className={`dsc-btn dsc-right-6 ${this.props.descramblerSettingRight === 6 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="6">6</button>
           <button
-            className="dsc-btn dsc-right-7"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 7 && 'green',
-              color: this.props.descramblerSettingRight === 7 && 'white',
-            }}
+            className={`dsc-btn dsc-right-7 ${this.props.descramblerSettingRight === 7 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="7">7</button>
           <button
-            className="dsc-btn dsc-right-8"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 8 && 'green',
-              color: this.props.descramblerSettingRight === 8 && 'white',
-            }}
+            className={`dsc-btn dsc-right-8 ${this.props.descramblerSettingRight === 8 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="8">8</button>
           <button
-            className="dsc-btn dsc-right-9"
-            style={{
-              backgroundColor: this.props.descramblerSettingRight === 9 && 'green',
-              color: this.props.descramblerSettingRight === 9 && 'white',
-            }}
+            className={`dsc-btn dsc-right-9 ${this.props.descramblerSettingRight === 9 && 'dsc-btn-sel'}`}
             name="descramblerSettingRight"
             onClick={this.props.handleChange}
             value="9">9</button>

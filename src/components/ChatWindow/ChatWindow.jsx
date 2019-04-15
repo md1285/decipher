10c6a6681c1b1535fff4c3c57e029b3c733ca202
Your Name <you@example.com>
2019-04-14 19:29:04 -0700

    resize chat page boxes

--- src/components/ChatWindow/ChatWindow.jsx
+++ src/components/ChatWindow/ChatWindow.jsx
@@ -6,21 +6,23 @@ class ChatWindow extends React.Component {
   render() {
     return (
       <div className={styles.ChatWindow}>
-
-        <h1 className={styles.h1}>ChatID: {this.props.match.params.id}</h1>
+        <div className={styles.h1Container}>
+          <h1 className={styles.h1}>DECIPHER</h1>
+        </div>
         <div id='chat-window-messages-container' className={styles.messagesContainer}>
           {this.props.messages.map((m, i) => (
             <div
 
               key={m._id}
-              style={
-                this.props.user._id === m.user
-                  ?
-                  { backgroundColor: 'red' }
-                  :
-                  { backgroundColor: 'green' }
-              }
-            >{m.userName}: {this.props.user._id === m.user  || this.props.descrambledForUser ? m.content : this.props.scrambledMessages[i]}</div>
+              style={{backgroundColor: 'white'}}
+              // style={
+              //   this.props.user._id === m.user
+              //     ?
+              //     { backgroundColor: 'red' }
+              //     :
+              //     { backgroundColor: 'green' }
+              // }
+            >{m.userName}: {this.props.user._id === m.user || this.props.descrambledForUser ? m.content : this.props.scrambledMessages[i]}</div>
           ))}
         </div>
         <form
